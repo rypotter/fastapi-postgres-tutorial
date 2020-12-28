6f8379d67403dc4b015e2883fe8ea5c6ec454ee6
rypotter <ry.potter@gmail.com>
2020-12-28 15:55:18 +0100

    'XX'

--- main.py
+++ main.py
@@ -7,16 +7,8 @@ from pydantic import BaseModel
 import os
 import urllib
 
-# host_server = os.environ.get('host_server', 'localhost')
-# db_server_port = urllib.parse.quote_plus(str(os.environ.get('db_server_port', '5432')))
-# database_name = os.environ.get('database_name', 'fastapi')
-# db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'postgres')))
-# db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', 'secret')))
-# ssl_mode = urllib.parse.quote_plus(str(os.environ.get('ssl_mode','prefer')))
-# DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(db_username, db_password, host_server, db_server_port, database_name, ssl_mode)
-# DATABASE_URL = 'postgresql://fazekasi:fazekasi@192.168.1.23:6432/jeszdb'
+
 DATABASE_URL = 'postgresql://postgres:postgres@centos7.datex:5432/pgdb'
-# DATABASE_URL = 'postgresql://player:player@pgtar.datex:5432/tardb'
 
 database = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
